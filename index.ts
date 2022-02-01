bfc8a6d492ae1af4ee1c0dbcb0d6839828fc7fae
Andrew Pepper <andrewpepper96@gmail.com>
2022-02-01 10:36:35 -0800

    docs: explaination

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ export function binomial(a: number, b: number, coeffs: Array<number>) {
   return (coeffs[0] * a + coeffs[1]) * (coeffs[2] * b + coeffs[3]);
 }
 
+// for app1
 export function app1(a: number, b: number) {
   return binomial(a, b, [1, 2, 1, 1]);
 }
 
+// for app2
 export function app2(a: number, b: number) {
   return binomial(a, b, [1, -1, 1, 2]);
 }
